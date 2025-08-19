adc1631fd9f0c9f298c52dca733503abe5b53b62
carter2099 <carter2099@pm.me>
2025-08-19 16:12:11 +0000

    migrate to docker compose

--- blog/release.sh
+++ blog/release.sh
@@ -6,7 +6,7 @@ echo 'Pulling changes from remote'
 git pull
 
 echo 'Bringing app down...'
-docker-compose -f docker-compose.prod.yml down
+RAILS_MASTER_KEY=$(cat config/master.key) docker compose -f docker-compose.prod.yml down
 docker image rm blog-web
 
 echo 'Starting app back up'
